841f1253c44907cd58dfb061468f438ae1355ec2
Akshay Kumar <akshay.kumar@sdiot.io>
2020-08-23 12:59:26 +0530

    Add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
-
-import { ExampleComponent } from 'react-confetch'
-import 'react-confetch/dist/index.css'
+import { useConfetch } from 'react-confetch'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const convertResponseToImageData = res => res.blob().then(image => URL.createObjectURL(image))
+
+  const config = {
+    url: 'https://avatars.githubusercontent.com',
+    endpoint: '/akshay-nm',
+    body: null,
+    method: 'GET',
+    timeoutDuration: 5000,
+    onResponse: convertResponseToImageData, // this is where you add logic to handle the response, any return value will be set as data, has to return a promise.
+    // onError: err => {}, // you can pass an error handler too, any return values will be assigned to error
+    // any error thrown is returned as error
+  }
+
+  const { data, loading, error, send } = useConfetch(config)
+
+  return (
+    <div>
+      <div>{data && <img src={data} alt='Avatar' />}</div>
+      <div>loading: {loading === null? 'ready': loading? 'yes' : 'no'}</div>
+      <div>error: {error? error.message: '-'}</div>
+      <div>
+        <button onClick={send}>Send a fetch request</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,11 @@
 import './index.css'
 
+import { ConfetchContext } from 'react-confetch'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ConfetchContext.Provider value={{}}>
+    <App />
+  </ConfetchContext.Provider>, document.getElementById('root'))
